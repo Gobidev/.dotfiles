9bdbbabb20c8e22f4a4fabae0df1c446fbda6944
Adrian Groh <adrian.groh@t-online.de>
2023-07-11 18:03:55 +0200

    nvim: remove unused lsps

--- .config/nvim/lua/gobidev/lsp/configs.lua
+++ .config/nvim/lua/gobidev/lsp/configs.lua
@@ -61,52 +61,51 @@ require('lspconfig').dockerls.setup(default_opts)
 -- require("lspconfig").golangci_lint_ls.setup(default_opts)
 -- require("lspconfig").gopls.setup(default_opts)
 require('lspconfig').jdtls.setup(default_opts)
-require('lspconfig').kotlin_language_server.setup(default_opts)
-require('lspconfig').gdscript.setup(default_opts)
+-- require('lspconfig').kotlin_language_server.setup(default_opts)
+-- require('lspconfig').gdscript.setup(default_opts)
 require('lspconfig').clangd.setup { capabilities = capabilities, on_attach = handlers.on_attach }
 require('lspconfig').taplo.setup(default_opts)
-require('lspconfig').ltex.setup(with_settings {
-    ltex = {
-        additionalRules = {
-            enablePickyRules = true,
-            motherTongue = 'de-De',
-        },
-    },
-})
-require('lspconfig').texlab.setup(with_settings {
-    texlab = {
-        build = {
-            args = {
-                '-xelatex',                 -- Build with xelatex
-                '-pdfxe',                   -- Build with xelatex
-                '-interaction=nonstopmode', -- Ignore errors
-                '-synctex=1',               -- Enable SyncTeX
-                '-pv',                      -- Open preview
-                '%f',
-            },
-            onSave = true, -- Build on save
-        },
-        forwardSearch = {
-            -- Use zathura for preview
-            executable = 'zathura',
-            args = { '--synctex-forward', '%l:1:%f', '%p' },
-        },
-
-        -- Set latexindent as formatter
-        bibtexFormatter = 'latexindent',
-        latexFormatter = 'latexindent',
-
-        -- Enable chktex lints
-        chktex = {
-            onOpenAndSave = true,
-            onEdit = true,
-        },
-    },
-})
-require('lspconfig').tsserver.setup(default_no_formatter)
-require('lspconfig').cssls.setup(default_no_formatter)
+-- require('lspconfig').ltex.setup(with_settings {
+--     ltex = {
+--         additionalRules = {
+--             enablePickyRules = true,
+--             motherTongue = 'de-De',
+--         },
+--     },
+-- })
+-- require('lspconfig').texlab.setup(with_settings {
+--     texlab = {
+--         build = {
+--             args = {
+--                 '-xelatex',                 -- Build with xelatex
+--                 '-pdfxe',                   -- Build with xelatex
+--                 '-interaction=nonstopmode', -- Ignore errors
+--                 '-synctex=1',               -- Enable SyncTeX
+--                 '-pv',                      -- Open preview
+--                 '%f',
+--             },
+--             onSave = true, -- Build on save
+--         },
+--         forwardSearch = {
+--             -- Use zathura for preview
+--             executable = 'zathura',
+--             args = { '--synctex-forward', '%l:1:%f', '%p' },
+--         },
+--
+--         -- Set latexindent as formatter
+--         bibtexFormatter = 'latexindent',
+--         latexFormatter = 'latexindent',
+--
+--         -- Enable chktex lints
+--         chktex = {
+--             onOpenAndSave = true,
+--             onEdit = true,
+--         },
+--     },
+-- })
+-- require('lspconfig').tsserver.setup(default_no_formatter)
+-- require('lspconfig').cssls.setup(default_no_formatter)
 require('lspconfig').html.setup(default_no_formatter)
-require('lspconfig').emmet_ls.setup(default_opts)
 require('lspconfig').jsonls.setup(default_no_formatter)
 require('lspconfig').typst_lsp.setup(default_opts)
 require('prettier').setup {
