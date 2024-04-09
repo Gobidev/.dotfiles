a1f458e5e6b3e26b3b04eecaf46d400f07e9a592
Adrian Groh <adrian.groh@t-online.de>
2024-04-09 15:47:04 +0200

    nvim: change haskell evaluate shortcut

--- .config/nvim/after/plugin/haskell.lua
+++ .config/nvim/after/plugin/haskell.lua
@@ -3,4 +3,4 @@ local bufnr = vim.api.nvim_get_current_buf()
 local opts = { noremap = true, silent = true, buffer = bufnr, }
 
 -- Evaluate all code snippets
-vim.keymap.set('n', '<space>ea', ht.lsp.buf_eval_all, opts)
+vim.keymap.set('n', '<space>le', ht.lsp.buf_eval_all, opts)
