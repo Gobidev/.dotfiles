21b4a4263f206a4377334efcc9511a36320c0d49
Adrian Groh <adrian.groh@t-online.de>
2025-10-24 14:34:00 +0200

    More nvim stuff

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -2,3 +2,22 @@ vim.lsp.enable("lua_ls")
 vim.lsp.enable("stylua")
 
 vim.lsp.enable("ansiblels")
+
+vim.lsp.config('rust_analyzer', {
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+})
+vim.lsp.enable('rust_analyzer')
+
+vim.lsp.enable('pyright')
+
+
+vim.diagnostic.config({
+  severity = { min = vim.diagnostic.severity.WARN },
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
+})
